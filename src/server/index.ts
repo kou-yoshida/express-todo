7d3e6a4709e420ed4d5508410c54be8d4830b796
kou-yoshida <kouyoshida28@gmail.com>
2024-01-30 23:16:10 +0900

    🗃️ prismaの繋ぎ込み、挙動確認など

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,22 +1,54 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/users", (req, res) => {
-  res.send(["user1", "user2", "user3"]);
+app.use("/users", async (req, res) => {
+  const name = "test";
+  const email = "test-email";
+  const passwordHash = "test-password";
+
+  await prisma.users.create({
+    data: {
+      name,
+      email,
+      password_hash: passwordHash,
+    },
+  });
+
+  const result = await prisma.users.findMany();
+  res.send(result);
 });
 
 app.use("/user/1", (req, res) => {
   res.send("user1");
 });
 
-// 特定のユーザーidのtodo一覧を取得する
+// 全てのtodoを取得する
+app.get("/todos", async (req, res) => {
+  const todos = await prisma.todos.findMany();
+  res.send(todos);
+});
 
-// 特定のユーザーのtodoを作成する
+// todoを作成する
+app.post("/todos", async (req, res) => {
+  // const { name, email, passwordHash } = req.body;
+  // const name = "test";
+  // const email = "test-email";
+  // const passwordHash = "test-password";
+  // prisma.todos.create({
+  //   data: {
+  //     name,
+  //     email,
+  //     password_hash: passwordHash,
+  //   },
+  // });
+});
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
